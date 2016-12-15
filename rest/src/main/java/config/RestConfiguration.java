026f3e655ef868c588a4228d859a2c9270a5a6fb
Venca24 <Vaclav.Zouzalik@seznam.cz>
2016-12-15 22:20:19 +0100

    Added home page into spring

--- rest/src/main/java/config/RestConfiguration.java
+++ rest/src/main/java/config/RestConfiguration.java
@@ -1,11 +1,18 @@
 package config;
 
 import controllers.MachineController;
+import javax.validation.Validator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import service.config.ServiceConfiguration;
 
 /**
@@ -16,5 +23,28 @@ import service.config.ServiceConfiguration;
 @Import(ServiceConfiguration.class)
 @ComponentScan(basePackageClasses = {MachineController.class})
 public class RestConfiguration extends WebMvcConfigurerAdapter {
+    
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+    
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+    
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
 }
---
+++ rest/src/main/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : home
+    Created on : 15.12.2016, 21:42:12
+    Author     : Václav Zouzalík
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Rental of construction machinery</title>
+    </head>
+    <body>
+        <h1>Welcome!</h1>
+    </body>
+</html>
